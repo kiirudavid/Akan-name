e831a995b3988bedf22a692f7419b9b157aedf65
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-17 18:33:46 +0300

    add date function

---
+++ js/node.js
@@ -0,0 +1,8 @@
+function fn () {
+    var d = new Date ();
+
+    var date = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+
+}
